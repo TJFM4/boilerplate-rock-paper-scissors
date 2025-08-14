354ab543c791bb858c6570ac628e56895f49d065
Tristan Martin <tjfm4@cam.ac.uk>
2025-08-14 08:15:35 -0400

    start

--- RPS.py
+++ RPS.py
@@ -1,5 +1,22 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+from collections import deque, defaultdict
+import random
+
+#Hyperparameters
+k = 2                                           #memory length (number of previous steps)
+a = 1                                           #laplacian smoothing for non-zero probabilities
+e = 0.04                                        #rate of randomness (avoids predictability)
+pad = "pad"                                     #used to pad history data when insufficient plays have occured
+
+counters = {"R": "P", "P": "S", "S": "R"}
+
+history = deque(maxlen=k)
+
+def current():
+    
+
+
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
